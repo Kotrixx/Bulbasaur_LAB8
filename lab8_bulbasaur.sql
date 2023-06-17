d2991792490bb5ceb406d7aa3aab4bd6dbb878fc
kotrix <a20206331@pucp.edu.pe>
2023-06-17 00:11:31 -0500

    creaScript

--- lab8_bulbasaur.sql
+++ lab8_bulbasaur.sql
@@ -23,6 +23,7 @@ CREATE TABLE IF NOT EXISTS `lab8_bulbasaur`.`Usuario` (
   `nombre` VARCHAR(80) NOT NULL,
   `apellido` VARCHAR(80) NOT NULL,
   `contrasenha` VARCHAR(64) NOT NULL,
+  `contrasenha_hashed` VARCHAR(64) NOT NULL,
   `status` VARCHAR(45) NOT NULL,
   `edad` INT NOT NULL,
   `codigo` INT NOT NULL,
@@ -46,7 +47,6 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `lab8_bulbasaur`.`Viaje` (
   `idViaje` INT NOT NULL AUTO_INCREMENT,
-  `precio` DECIMAL(10,2) NOT NULL,
   `fechaViaje` DATETIME NOT NULL,
   `fechaReserva` DATETIME NOT NULL,
   `costoTotal` DECIMAL(10,2) NOT NULL,
@@ -77,18 +77,12 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 
-ALTER TABLE `lab8_bulbasaur`.`Usuario`
-ADD COLUMN `contrasenha_hashed` VARCHAR(64) NOT NULL;
-
-
+USE `lab8_bulbasaur` ;
 INSERT INTO `lab8_bulbasaur`.`Usuario` (`correo`, `nombre`, `apellido`,`contrasenha`, `contrasenha_hashed`, `status`, `edad`, `codigo`, `especialidad`)
-VALUES ('usuario1@pucp.edu.pe', 'Usuario', 'Uno','password1', SHA2('password1', 256), 'Normal', 20, 12345678, 'Especialidad 1');
-
-INSERT INTO `lab8_bulbasaur`.`Usuario` (`correo`, `nombre`, `apellido`,`contrasenha`, `contrasenha_hashed`, `status`, `edad`, `codigo`, `especialidad`)
-VALUES ('usuario2@pucp.edu.pe', 'Usuario', 'Dos','password2', SHA2('password2', 256), 'Normal', 25, 23456789, 'Especialidad 2');
-
-INSERT INTO `lab8_bulbasaur`.`Usuario` (`correo`, `nombre`, `apellido`,`contrasenha`, `contrasenha_hashed`, `status`, `edad`, `codigo`, `especialidad`)
-VALUES ('usuario3@pucp.edu.pe', 'Usuario', 'Tres','password3', SHA2('password3', 256), 'Silver', 28, 34567890, 'Especialidad 3');
+VALUES ('usuario1@pucp.edu.pe', 'Usuario', 'Uno','password1', SHA2('password1', 256), 'Normal', 20, 12345678, 'Telecomunicaciones'),
+ ('usuario3@pucp.edu.pe', 'Usuario3', 'Tres','password3', SHA2('password3', 256), 'Silver', 28, 34567890, 'Telecomunicaciones'),
+('usuario2@pucp.edu.pe', 'Usuario2', 'Dos','password2', SHA2('password2', 256), 'Gold', 25, 23456789, 'Ingeniería de Telecomunicaciones'),
+ ('usuario4@pucp.edu.pe', 'Usuario4', 'Tres','password4', SHA2('password4', 256), 'Platinum', 28, 12312456, 'Telecomunicaciones');
 
 INSERT INTO `lab8_bulbasaur`.`Seguro` (`nombre`) VALUES 
 ('Rimac'),
@@ -96,7 +90,8 @@ INSERT INTO `lab8_bulbasaur`.`Seguro` (`nombre`) VALUES
 ('La Positiva'),
 ('Seguro Internacional');
 
-INSERT INTO `lab8_bulbasaur`.`Viaje` ( `precio`, `fechaViaje`, `fechaReserva`, `costoTotal`, `precioBoletO`, `cantBoletos`, `Seguro_idSeguro`, `ciudadOrigen`, `ciudadDestino`, `Usuario_idUsuario`) VALUES
-( 0.00, '2024-06-15 09:00:00', '2024-05-30 14:30:00', 0.00, 0.00, 0, 1, 'Lima', 'Cusco', 1),
-( 0.00, '2024-07-10 14:30:00', '2024-06-01 09:15:00', 0.00, 0.0, 0, 2, 'Lima', 'Arequipa', 2),
-( 320.75, '2024-09-25 10:30:00', '2024-08-10 13:20:00', 360.50, 39.75, 4, 2, 'Lima', 'Piura', 3);
+INSERT INTO `lab8_bulbasaur`.`Viaje` (`idViaje`, `fechaViaje`, `fechaReserva`, `costoTotal`, `precioBoletO`, `cantBoletos`, `Seguro_idSeguro`, `ciudadOrigen`, `ciudadDestino`, `Usuario_idUsuario`) VALUES
+( 12114478, '2024-06-15 09:00:00', '2024-05-30 14:30:00', 0.00, 0.00, 0, 1, 'Lima', 'Cusco', 1),
+( 87654321, '2024-07-10 14:30:00', '2024-06-01 09:15:00', 374.50, 0.00, 1, 2, 'Lima', 'Arequipa', 2),
+( 13656244, '2024-09-25 10:30:00', '2024-08-10 13:20:00', 1278.90, 0.00, 3, 3, 'Lima', 'Piura', 3),
+( 46781457, '2024-06-25 10:30:00', '2024-08-26 13:20:00', 17842.90, 0.00, 15, 4, 'Lima', 'Trujillo', 4);
